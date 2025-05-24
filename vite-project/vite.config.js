9f280512bb8af783dc05bc34e86044469124ff9f
unknown <anbuk6722@gmail.com>
2025-05-24 09:28:30 -0700

    Fix vite.config.js and update package.json for deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/anbu_portfolio/', // ✅ Required for GitHub Pages
-  plugins: [react(),tailwindcss()],
-
+  base: '/anbu_portfolio/', // ✅ Change this if your repo name is different
+  plugins: [react()]
 });
